2c066158a33835dd15d9c720c7b563217c8530cf
Jan Dub <dubstolcova@gmail.com>
2022-10-27 15:37:34 +0200

    trying other method

--- Algorithmization/array.cpp
+++ Algorithmization/array.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 const int size=1000;
 typedef float type[size];
+typedef string saving[size];
 
 int main() {
     type field;
+    saving savingField;
     int number =0;
 
     cout<<"input number "<<number+1<<":";
@@ -39,6 +41,7 @@ int main() {
     for(int i=0;i<number;i++){
         if(field[i]<average){
             underAverageField[index]+= field[i];
+            savingField[i]+=to_string(field[i])+", ";
             index++;
         }
     }
@@ -50,5 +53,6 @@ int main() {
     cout<<"average is : "<<average<<endl;
     cout<<"sum is : "<<sum<<endl;
     cout<<"number of under average numbers in array : "<<underAverage<<endl;
+    cout<<"array of under average numbers : "<<savingField<<endl;
     return 0;
 }
