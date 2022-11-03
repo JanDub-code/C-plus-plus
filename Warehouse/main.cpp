418381e35261ef96010c3e05be23fa94b268a87b
Jan Dub <dubstolcova@gmail.com>
2022-11-03 11:48:53 +0100

    main

---
+++ Warehouse/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "section.h"
+
+int main() {
+    Container * container = new Container("A1",100);
+    container->newProduct("Sword");
+    container->printInfo();
+    std::cout<<std::endl;
+    container->deleteProduct("Sword");
+    container->printInfo();
+    std::cout<<std::endl;
+    
+    Section * section = new Section("A");
+    section->addEmptyContainer("A1");
+    section->addProductToContainer("Sword","A1");
+    section->printStatistics();
+    
+    delete section;
+    delete container;
+    
+    return 0;
+}
+
