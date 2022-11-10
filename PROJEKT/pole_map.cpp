b85d24019079006e2409d8188f99ee10ed1022d0
Jan Dub <dubstolcova@gmail.com>
2022-11-10 16:24:57 +0100

    created blueprint documentation for room, matrix

---
+++ PROJEKT/pole_map.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Room{
+    //generate room witch choosable size which we can show in main function
+    //we will have mupltiple predefined rooms witch we can choose from
+    //we can have a player a potions and monsters in room
+
+
+
+
+
+
+}
+
+class MatrixOfRooms{
+    //generate vector matrix in which we can save individual rooms in specific points
+    vector<vector<Room>> m_matrix;
+    //and create a void that allows us traveling between them
+    
+
+}
+
+int main(){
+
+    return 0;
+}
