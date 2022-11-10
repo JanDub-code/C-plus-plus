589545195fa9e83be416530b0b124fd40d5f681f
Jan Dub <dubstolcova@gmail.com>
2022-11-10 16:23:56 +0100

    created class player and some documentation

---
+++ PROJEKT/Player.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Hero{
+    // create chosable class class (warrior,archer,assasin)
+    bool m_isAlive;
+    float m_health;
+    float m_strength;
+    float m_dexterity;
+
+}
+
+
+int main() {
+
+    return 0;
+}
