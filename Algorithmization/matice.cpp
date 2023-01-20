c135e3fe6ebed68d4c55f0a14794f79ed40ac57c
Jan Dub <dubstolcova@gmail.com>
2023-01-20 23:13:40 +0100

    training

--- Algorithmization/matice.cpp
+++ Algorithmization/matice.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+const int maxRozsah = 100;
+typedef int typPole[maxRozsah];
+typedef typPole typMatice[maxRozsah];
+void vypisPole(typPole pole, int pocet){
+  for(int i = 0; i<pocet; i++){
+      cout<< setw(5)<< pole[i];
+  }
+  cout<<endl;
+}
+void vypisMatici(typMatice pole, int radky, int sloupce){
+  for(int i = 0; i<radky; i++){
+      vypisPole(pole[i],sloupce);
+  }
+  cout<<endl;
+}
 
-
-int matice[100][50];
-
-char PR [60][20];
-
-int main() {
-
+int main(){
+    typMatice mat;
+    int N;
+    cin>>N;
+    for(int r=0; r<N;r++)
+        for(int s=0; s<N;s++)
+            mat[r][s]=1;
+    for(int r=0; r<N;r++)
+        mat[r][r]=0;
+    for(int r=0; r<N;r++)
+        mat[r][N-1-r]=2;
+    for(int r=0; r<N;r++)
+        for(int s=0; s<N;s++)
+            mat[s][r]=r*N+s +1;
+    vypisMatici(mat,N,N);
+    
     
     return 0;
 }
