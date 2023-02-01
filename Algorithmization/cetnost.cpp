442054e0e8806a99eca17d5d19edb56e21aff4fd
Jan Dub <dubstolcova@gmail.com>
2023-02-01 14:40:59 +0100

    upravit

---
+++ Algorithmization/cetnost.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    struct cetnost{
+        char cet;
+        int pocet;
+    }
+    
+    string ahoj="sjkasuisadCSDHFF54155454";
+    for(int i=0;i<ahoj.length();i++){
+        if(ahoj[i]>='a'&& ahoj[i]<='z'){
+            u=new cetnost(ahoj[i],)pocet
+    }
+
+    }    
+
+    for(int i=0;i<N;i++){
+        cout<<vycet[i];
+    }
+    return 0;
+}
